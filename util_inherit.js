836fffe8606c4ba29ede1d25c866d4eb5444e890
Mengying Long <531107066@qq.com>
2018-02-08 22:50:52 +0800

    Day4 get post route server file operation web client web server

---
+++ util_inherit.js
@@ -0,0 +1,23 @@
+var util = require('util'); 
+function Base() { 
+    this.name = 'base'; 
+    this.base = 1991; 
+    this.sayHello = function() { 
+    console.log('Hello ' + this.name); 
+    }; 
+} 
+Base.prototype.showName = function() { 
+    console.log(this.name);
+}; 
+function Sub() { 
+    this.name = 'sub'; 
+} 
+util.inherits(Sub, Base); 
+var objBase = new Base(); 
+objBase.showName(); 
+objBase.sayHello(); 
+console.log(objBase); 
+var objSub = new Sub(); 
+objSub.showName(); 
+//objSub.sayHello(); 不会被继承，会报错；仅仅继承在base在原型中定义的部分（这里是showname函数）
+console.log(objSub); 
